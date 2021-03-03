a4a58db37b272d5daae6a5da142d53ff03a69374
Colin Unger <unger@stanford.edu>
2021-03-03 02:00:29 -0800

    Update build config for sapling

--- env.sh
+++ env.sh
@@ -1,6 +1,5 @@
-source "$HOME/FlexFlow/env.sh"
-ml restore taso
-ml gdb 
+#source "$HOME/FlexFlow/env.sh"
+#ml restore taso
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/TASO/build/"
 export LIBRARY_PATH="$LIBRARY_PATH:$HOME/TASO/build/"
 export TASO_HOME="$HOME/TASO"
